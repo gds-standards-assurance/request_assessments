6286e571ec501e6071842a0de881acd3f4a25f53
Abisola Fatokun <abisola@fatokun.com>
2017-01-05 16:48:13 +0000

    validation
    introducing the validator helper and validation for the forms

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 var express = require('express')
 const util = require('util')
+var validator = require('validator');
 var getDatesInMonth = require('../lib/helper.js')
 
 var NotifyClient = require('notifications-node-client').NotifyClient
@@ -18,6 +19,8 @@ var router = express.Router()
 var sess;
 var data;
 
+//TODO extract session checker
+
 // Route index page
 router.get('/', function (req, res) {
   res.render('index')
@@ -31,6 +34,7 @@ router.get('/start', function (req, res) {
 
 router.post('/stage', function(req, res) {
   sess = req.session
+  var errors = {}
 
   if (sess.user_info) {
 
@@ -42,55 +46,96 @@ router.post('/stage', function(req, res) {
     sess.user_info = _data
   }
 
-  var name = req.body.service_manager_name
-  var email = req.body.service_manager_email
-  var dept = req.body.service_manager_department
+  var name = validator.trim(req.body.service_manager_name)
+  var email = validator.trim(req.body.service_manager_email)
+  var dept = validator.trim(req.body.service_manager_department)
+
+  //check if any of these items is invalid
+  if(!validator.isEmail(email)) {
+    errors.email = "You have entered an invalid email address"
+  }
+
+  if(validator.isEmpty(name)) {
+    errors.name = "Your name should not be empty"
+  }
+
+  if(validator.isEmpty(dept)) {
+    errors.dept = "Your must provide your department or agency name"
+  }
 
   data = { name: name, email: email, dept: dept }
   sess.user_info = data
 
-  console.log('\nService Manager details: ' + name + ', ' + email)
-  res.render('stage')
+  if (Object.keys(errors).length > 0) {
+
+    console.log("\nErrors found in the submission: " + util.inspect(errors))
+    res.render('start', {errors: errors, user_info: sess.user_info})
+
+  } else {
+    res.render('stage')
+  }
 })
 
 router.post('/pick-date', function (req, res) {
 
+  var errors = {}
   sess = req.session
 
   if (sess.user_info){
-    var service_name = req.body.service_name
+    var service_name = validator.trim(req.body.service_name)
     var assessment_stage = req.body.assessment_stage
+
+    if(validator.isEmpty(service_name)) {
+      errors.service_name = "Your must provide your service name"
+    }
+
+    if(!assessment_stage || typeof assessment_stage === 'undefined') {
+      errors.assessment_stage = "Please choose the assessment stage"
+    }
+
     sess.user_info.assessment_stage = assessment_stage
     sess.user_info.service_name = service_name
+
+    if (Object.keys(errors).length > 0) {
+
+      console.log("\nErrors found in the submission: " + util.inspect(errors))
+      return res.render('stage', {errors: errors, user_info: sess.user_info})
+
+    }
+
+    var today = new Date();
+    var earliestdate = new Date();
+    earliestdate.setDate(today.getDate() + 28);
+
+    var weekday = earliestdate.getDay();
+    if (weekday === 0) {  //Sunday add 1
+      earliestdate.setDate(earliestdate.getDate() + 1);
+    }
+
+    if (weekday === 6) {  //Saturday add 2
+      earliestdate.setDate(earliestdate.getDate() + 2);
+    }
+
+    sess.user_info.earliestdate = earliestdate;
+    var _month = earliestdate.getMonth();
+
+    var months = [mlist[_month], mlist[_month + 1], mlist[_month + 2], mlist[_month + 3]]
+    sess.user_info.months = months
+
+    console.log('\n Session details' + util.inspect(sess))
+    console.log('\n Months: ' + util.inspect(months))
+    res.render('pick-date', {months: months})
+
   } else {
     //throw user back to the start page
     console.log('\nSession does not exist. go back to the start page')
-    return res.render('start', {error: 'We cannot find the details of your request. You will need to start again'})
+    res.render('start', {error: 'We cannot find the details of your request. You will need to start again'})
   }
 
-  var today = new Date();
-  var earliestdate = new Date();
-  earliestdate.setDate(today.getDate() + 28);
-
-  var weekday = earliestdate.getDay();
-  if (weekday === 0) {  //Sunday add 1
-    earliestdate.setDate(earliestdate.getDate() + 1);
-  }
-
-  if (weekday === 6) {  //Saturday add 2
-    earliestdate.setDate(earliestdate.getDate() + 2);
-  }
-
-  sess.user_info.earliestdate = earliestdate;
-  var _month = earliestdate.getMonth();
-
-  var months = [mlist[_month], mlist[_month + 1], mlist[_month + 2], mlist[_month + 3]]
-  console.log('\n Session details' + util.inspect(sess))
-  console.log('\n Months: ' + util.inspect(months))
-  res.render('pick-date', {months: months})
 })
 
 router.post('/pick-day', function(req, res){
+  var errors = {}
   sess = req.session
   var _month, _monthnum, _startdate;
 
@@ -100,6 +145,18 @@ router.post('/pick-day', function(req, res){
     _monthnum = sess.user_info.month_index
     _startdate = sess.user_info.earliestdate
     sess.user_info.assessment_month = assessment_month
+
+    if(!assessment_month || typeof assessment_month === 'undefined') {
+      errors.assessment_month = "Please choose the month"
+    }
+
+    if (Object.keys(errors).length > 0) {
+
+      console.log("\nErrors found in the submission: " + util.inspect(errors))
+      return res.render('pick-date', {errors: errors, user_info: sess.user_info, months: sess.user_info.months})
+
+    }
+
   } else {
     //throw user back to the start page
     console.log('\nSession does not exist. go back to the start page')
@@ -107,6 +164,7 @@ router.post('/pick-day', function(req, res){
   }
 
   var dates = getDatesInMonth(mlist.indexOf(_month), _startdate) //get dates in Month
+  sess.user_info.tmpdates = dates
   console.log('\n dates in feb: ' + util.inspect(dates))
 
   console.log('\n Session details' + util.inspect(sess))
@@ -114,11 +172,24 @@ router.post('/pick-day', function(req, res){
 })
 
 router.post('/pick-time', function(req, res){
+  var errors = {}
   sess = req.session
 
   if (sess.user_info){
     var assessment_date = req.body.assessment_date
     sess.user_info.assessment_date = assessment_date
+
+    if(!assessment_date || typeof assessment_date === 'undefined') {
+      errors.assessment_date = "Please choose the date"
+    }
+
+    if (Object.keys(errors).length > 0) {
+
+      console.log("\nErrors found in the submission: " + util.inspect(errors))
+      return res.render('pick-day', {errors: errors, user_info: sess.user_info, month_picked: sess.user_info.assessment_month, dates: sess.user_info.tmpdates})
+
+    }
+
   } else {
     //throw user back to the start page
     console.log('\nSession does not exist. go back to the start page')
@@ -130,11 +201,24 @@ router.post('/pick-time', function(req, res){
 })
 
 router.post('/summary', function(req, res) {
+  var errors = {}
   sess = req.session
 
   if (sess.user_info){
     var assessment_time = req.body.assessment_time
     sess.user_info.assessment_time = assessment_time
+
+    if(!assessment_time || typeof assessment_time === 'undefined') {
+      errors.assessment_time = "Please choose the time"
+    }
+
+    if (Object.keys(errors).length > 0) {
+
+      console.log("\nErrors found in the submission: " + util.inspect(errors))
+      return res.render('pick-time', {errors: errors, picked_date: sess.user_info.assessment_date})
+
+    }
+
   } else {
     //throw user back to the start page
     console.log('\nSession does not exist. go back to the start page')
